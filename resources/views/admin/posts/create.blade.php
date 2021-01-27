33ef3cf32877a5d7fa8bddbd19309f20c761cf42
ujangaris <ujangaja@gmail.com>
2021-01-27 20:21:06 +0700

    update semua data ke dalam blog

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('admin.includes.errors')
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+          Create a new post        
+      </div>
+      <div class="panel-body">
+        <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
+          {{ csrf_field() }}
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input type="text" name="title" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="featured">Featured</label>
+              <input type="file" name="featured" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="featured">Content</label>
+              <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+              <div class="text-center">
+                <button type="submit" class="btn btn-success">Store post</button>
+              </div>
+            </div>
+        </form>
+      </div>
+    </div>
+@stop
