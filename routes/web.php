c4db5c6b6799045156e189852f7f4f06536f0b6c
ujangaris <ujangaja@gmail.com>
2021-01-27 20:38:34 +0700

    update semua data ke blog branch gagal

--- routes/web.php
+++ routes/web.php
@@ -5,43 +5,89 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/new',[
-     
+Route::get('/new', [
+
     'uses' => 'PagesController@new'
-    
-    ]);
-    
-    Route::get('/todos',[
-        
-        'uses' => 'TodosController@index',
-        'as'   => 'todos'
+
+]);
+
+Route::get('/todos', [
+
+    'uses' => 'TodosController@index',
+    'as'   => 'todos'
 ]);
 
 
-Route::get('/todo/delete/{id}',[
-     
+Route::get('/todo/delete/{id}', [
+
     'uses' => 'TodosController@delete',
     'as'   => 'todo.delete'
 ]);
 
-Route::get('/todo/update/{id}',[
-     
+Route::get('/todo/update/{id}', [
+
     'uses' => 'TodosController@update',
     'as'   => 'todo.update'
 ]);
 
-Route::post('/todo/save/{id}',[
-     
+Route::post('/todo/save/{id}', [
+
     'uses' => 'TodosController@save',
     'as'   => 'todo.save'
 ]);
 
-Route::post('/create/todo',[
-     
+Route::post('/create/todo', [
+
     'uses' => 'TodosController@store'
 ]);
-Route::get('/todos/completed/{id}',[
-     
+Route::get('/todos/completed/{id}', [
+
     'uses' => 'TodosController@completed',
     'as'   => 'todos.completed'
 ]);
+// Authentication
+Auth::routes();
+
+
+// Post
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+
+    Route::get('/home', [
+        'uses'  => 'HomeController@index',
+        'as'    => 'home'
+    ]);
+    Route::get('/post/create', [
+        'uses'   => 'PostsController@create',
+        'as'    => 'post.create'
+    ]);
+    Route::post('/post/store', [
+        'uses'   => 'PostsController@store',
+        'as'    => 'post.store'
+    ]);
+    // category
+    Route::get('/category/create', [
+        'uses'   => 'CategoriesController@create',
+        'as'    => 'category.create'
+    ]);
+    Route::post('/category/store', [
+        'uses'   => 'CategoriesController@store',
+        'as'    => 'category.store'
+    ]);
+
+    Route::get('/categories', [
+        'uses'   => 'CategoriesController@index',
+        'as'    => 'categories'
+    ]);
+    Route::get('/category/edit/{id}', [
+        'uses'   => 'CategoriesController@edit',
+        'as'    => 'category.edit'
+    ]);
+    Route::post('/category/update/{id}', [
+        'uses'   => 'CategoriesController@update',
+        'as'    => 'category.update'
+    ]);
+    Route::get('/category/delete/{id}', [
+        'uses'   => 'CategoriesController@destroy',
+        'as'    => 'category.delete'
+    ]);
+});
