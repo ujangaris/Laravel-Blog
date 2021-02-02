96f09e01472dcfce650a2f9944fdc2d40b2e987e
ujangaris <ujangaja@gmail.com>
2021-02-02 14:39:28 +0700

    065 | Single blog post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -60,3 +60,17 @@
       </div>
     </div>
 @stop
+
+<!-- include summernote css/js -->
+@section('styles')
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+@stop
+@section('scripts')
+     
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        $('#content').summernote();
+      });
+    </script>
+@stop
