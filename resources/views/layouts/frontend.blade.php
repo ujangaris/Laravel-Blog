11a5fe69250ad3079da27fb4ac5cfa59041aed4e
ujangaris <ujangaja@gmail.com>
2021-02-23 18:54:53 +0700

    072 | AddThis installation into Blog

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -144,6 +144,8 @@
 
 
 <!-- ...end JS Script -->
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-6034e9d4591772cf"></script>
 
 </body>
 </html>
